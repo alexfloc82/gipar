4769f537086084df27cc869adbc2a1c2d3fcf2cc
alexfloc82 <alexandre.floquet@gmail.com>
2017-10-22 21:19:21 +0200

    prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCVF4Zj0-yw_GTNMvsIl5CktQ_rNfdPbQw",
+    authDomain: "gipar-ads.firebaseapp.com",
+    databaseURL: "https://gipar-ads.firebaseio.com",
+    projectId: "gipar-ads",
+    storageBucket: "",
+    messagingSenderId: "241057780571"
+  }
 };
